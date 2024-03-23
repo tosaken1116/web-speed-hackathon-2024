975ff93d72304d7372b4b1be583025b3ba8509af
tosaken1116 <nagato20021122@gmail.com>
2024-03-23 11:16:13 +0900

    import icons only used

--- workspaces/app/src/features/icons/components/SvgIcon.tsx
+++ workspaces/app/src/features/icons/components/SvgIcon.tsx
@@ -1,10 +1,22 @@
-import * as Icons from '@mui/icons-material';
-
+import Close from '@mui/icons-material/Close';
+import NavigateNext from '@mui/icons-material/NavigateNext';
+import Favorite from '@mui/icons-material/Favorite';
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
+import Search from '@mui/icons-material/Search';
+import ArrowBack from '@mui/icons-material/ArrowBack';
 type Props = {
   color: string;
   height: number;
   type: keyof typeof Icons;
   width: number;
+};
+const Icons = {
+  Close: Close,
+  NavigateNext: NavigateNext,
+  Favorite: Favorite,
+  FavoriteBorder: FavoriteBorder,
+  Search: Search,
+  ArrowBack: ArrowBack,
 };
 
 export const SvgIcon: React.FC<Props> = ({ color, height, type, width }) => {
