a5ce9d004e07e6927e6de102d2a49e4d825eb1d1
tosaken1116 <nagato20021122@gmail.com>
2025-03-14 01:31:30 +0900

    refactor: update header handling in compressMiddleware for correct encoding detection

--- workspaces/client/src/serviceworker/index.ts
+++ workspaces/client/src/serviceworker/index.ts
@@ -2,7 +2,6 @@
 import PQueue from 'p-queue';
 
 import { transformJpegXLToBmp } from './transformJpegXLToBmp';
-import { zstdFetch as fetch } from './zstdFetch';
 
 // ServiceWorker が負荷で落ちないように並列リクエスト数を制限する
 const queue = new PQueue({
