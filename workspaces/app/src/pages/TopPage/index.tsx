84ea5f6bd7098c59b6ddff7187f17a2dc3f7ab17
tosaken1116 <nagato20021122@gmail.com>
2025-03-14 04:18:03 +0900

    refactor: remove console.log statement from TopPage component

--- workspaces/app/src/pages/TopPage/index.tsx
+++ workspaces/app/src/pages/TopPage/index.tsx
@@ -24,7 +24,6 @@ const TopPage: React.FC = () => {
   const pickupA11yId = useId();
   const rankingA11yId = useId();
   const todayA11yId = useId();
-  console.log({ featureList });
   return (
     <Flex align="flex-start" direction="column" gap={Space * 2} justify="center" pb={Space * 2}>
       <Box as="header" maxWidth="100%" width="100%">
