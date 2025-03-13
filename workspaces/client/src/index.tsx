8664300208f4e7939c22870b8407d5ef392f8694
tosaken1116 <nagato20021122@gmail.com>
2025-03-14 04:40:23 +0900

    fix: enable service worker registration in index.tsx

--- workspaces/client/src/index.tsx
+++ workspaces/client/src/index.tsx
@@ -10,7 +10,7 @@ import { ClientApp } from '@wsh-2024/app/src/index';
 import { registerServiceWorker } from './utils/registerServiceWorker';
 
 const main = async () => {
-  // await registerServiceWorker();
+  await registerServiceWorker();
 
   $(document).ready(() => {
     if (window.location.pathname.startsWith('/admin')) {
